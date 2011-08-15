82a41538250fe39a124929cc2f12c924e5e1b671
donnymac <don@onlineone.com.au>
2011-08-15 16:46:58 +1000

    sdasd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TestRest2::Application.routes.draw do
-  root :to => "home#index"
+  root :to => "home#index.html"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
